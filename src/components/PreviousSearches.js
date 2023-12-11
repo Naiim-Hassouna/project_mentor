b342d7807c931ef6f97884f90aeac1feb3492b25
Naiim <naiimhassouna0@gmail.com>
2023-12-11 15:47:12 +0200

    previous searches array is now modified as user searches

--- src/components/PreviousSearches.js
+++ src/components/PreviousSearches.js
@@ -1,86 +1,101 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faSearch, faFilter } from "@fortawesome/free-solid-svg-icons"
-import TagsInput from "./TagsInput"
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import TagsInput from "./TagsInput";
 
-export default function PreviousSearches(){
-    const searches = ['web', 'java', 'c++', 'python', 'c#', 'arduino', 'dart'] 
-
-
-
+function PreviousSearches() {
+  const [searches, setSearches] = useState(
+    JSON.parse(localStorage.getItem("previousSearches")) || []
+  );
   const [isTabOpen, setIsTabOpen] = useState(false);
 
   const toggleTab = () => {
     setIsTabOpen(!isTabOpen);
-  }
-        return (
-            <div className="previous-searches section">
-                <h2>Previous Searches</h2>
-                <div className="previous-searches-container">
-                    { searches.map((search, index) => (<div key={index} style={{animationDelay: index * .1 + "s"}} className="search-item">
-                        {search}
-                    </div>)) }
-                </div>
+  };
 
-                <div>
-                <h2>Enter Search Tags:</h2>
-                    <div className="tagSearchContainer">
-                        <TagsInput/>
-                        <button className="tags-search-btn" onClick={toggleTab}>
-                            <FontAwesomeIcon icon={faFilter} />
-                        </button>
-                        <button className="tags-search-btn">
-                            <FontAwesomeIcon icon={faSearch} />
-                        </button>
-                    </div>
-                    <div>
-                            {isTabOpen && (
-                        
-                            <div className="tab-filter">
-                                <div className="column-filter">
-                                    <strong>Size:</strong>
-                                    <br />
-                                    <label>
-                                    <input type="radio" name="size" value="mini" /> Mini
-                                    </label>
-                                    <br />
-                                    <label>
-                                    <input type="radio" name="size" value="standard" /> Standard
-                                    </label>
-                                    <br />
-                                    <label>
-                                    <input type="radio" name="size" value="enterprise" /> Enterprise
-                                    </label>
-                                    <br />
-                                    <label>
-                                    <input type="radio" name="size" value="any" /> Any
-                                    </label>
-                                </div>
-                                <div className="column-filter">
-                                    <strong>Difficulty:</strong>
-                                    <br />
-                                    <label>
-                                    <input type="radio" name="difficulty" value="beginner" /> Beginner
-                                    </label>
-                                    <br />
-                                    <label>
-                                    <input type="radio" name="difficulty" value="moderate" /> Moderate
-                                    </label>
-                                    <br />
-                                    <label>
-                                    <input type="radio" name="difficulty" value="advanced" /> Advanced
-                                    </label>
-                                    <br />
-                                    <label>
-                                    <input type="radio" name="size" value="any" /> Any
-                                    </label>
-                                </div>
-                            </div>
-                        )}</div>
-                </div>
+  const handleSearch = (tags) => {
+    // Update the searches array, keeping a maximum of 8 items
+    const updatedSearches = [...searches.slice(-5), ...tags].slice(-8);
+    setSearches(updatedSearches);
+    // Save to local storage
+    localStorage.setItem("previousSearches", JSON.stringify(updatedSearches));
+  };
+
+  useEffect(() => {
+    // This effect is just to demonstrate the component mounting
+    console.log("Component mounted");
+  }, []);
+
+  return (
+    <div className="previous-searches section">
+      <div className="title1"><h2>Previous Searches: </h2></div>
+      <div className="previous-searches-container">
+        {searches.map((search, index) => (
+          <div
+            key={index}
+            style={{ animationDelay: index * 0.1 + "s" }}
+            className="search-item"
+          >
+            {search}
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <h2>Enter Search Tags:</h2>
+        <div className="tagSearchContainer">
+          <TagsInput onSearch={handleSearch} />
+          <button className="tags-search-btn" onClick={toggleTab}>
+            <FontAwesomeIcon icon={faFilter} />
+          </button>
+        </div>
+        <div>
+          {isTabOpen && (
+            <div className="tab-filter">
+              <div className="column-filter">
+                <strong>Size:</strong>
+                <br />
+                <label>
+                  <input type="radio" name="size" value="mini" /> Mini
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="size" value="standard" /> Standard
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="size" value="enterprise" /> Enterprise
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="size" value="any" /> Any
+                </label>
+              </div>
+              <div className="column-filter">
+                <strong>Difficulty:</strong>
+                <br />
+                <label>
+                  <input type="radio" name="difficulty" value="beginner" /> Beginner
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="difficulty" value="moderate" /> Moderate
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="difficulty" value="advanced" /> Advanced
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="difficulty" value="any" /> Any
+                </label>
+              </div>
             </div>
-                
-                
-                
-    );
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default PreviousSearches;
--- src/components/TagsInput.js
+++ src/components/TagsInput.js
@@ -1,35 +1,49 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-export default TagsInput;
+function TagsInput({ onSearch }) {
+  const [tags, setTags] = useState([]);
 
-function TagsInput() {
-  const[tags, setTags]= useState([])
-
-  function handleKeyDown(e){
-    if (e.key !== 'Enter') return
-    const value = e.target.value
-    if (!value.trim()) return
-    setTags([...tags, value])
-    e.target.value= ''
+  function handleKeyDown(e) {
+    if (e.key !== 'Enter') return;
+    const value = e.target.value;
+    if (!value.trim()) return;
+    setTags([...tags, value]);
+    e.target.value = '';
   }
 
-function removeTag(index)  {
-  setTags(tags.filter((el, i) => i !== index))
-}
+  function removeTag(index) {
+    setTags(tags.filter((el, i) => i !== index));
+  }
 
   return (
     <>
-      
-      
       <div className="tags-input-container">
         {tags.map((tag, index) => (
           <div className="tag-item" key={index}>
             <span className="text">{tag}</span>
-            <span className="close" onClick={() => removeTag(index)}>&times;</span>
-        </div>
+            <span className="close" onClick={() => removeTag(index)}>
+              &times;
+            </span>
+          </div>
         ))}
-        <input onKeyDown={handleKeyDown} type="text" className="tags-input" placeholder=" Add a tag..." />
+        <input
+          onKeyDown={handleKeyDown}
+          type="text"
+          className="tags-input"
+          placeholder=" Add a tag..."
+        />
       </div>
+      <button
+        onClick={() => onSearch(tags)}
+        className="tags-search-btn"
+        // You can add more styles or icons for the search button
+      >
+            <FontAwesomeIcon icon={faSearch} />
+      </button>
     </>
   );
 }
+
+export default TagsInput;
